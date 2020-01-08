1a5d70e6f4947c370d3d1fb48470a1c11900f23c
Max <maxmeuer@me.com>
2020-01-08 18:49:19 +0100

    Updates

---
+++ precache-manifest.8a84ae27c2e6d07536ea47d38b16cfe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76018aa059280367ff08948793aa5ab",
+    "url": "/seminar-proto-2/index.html"
+  },
+  {
+    "revision": "33420591b7fbb36d5d0b",
+    "url": "/seminar-proto-2/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5c997331670f94f327a9",
+    "url": "/seminar-proto-2/static/css/main.b55cbedb.chunk.css"
+  },
+  {
+    "revision": "33420591b7fbb36d5d0b",
+    "url": "/seminar-proto-2/static/js/2.bf78b98a.chunk.js"
+  },
+  {
+    "revision": "5c997331670f94f327a9",
+    "url": "/seminar-proto-2/static/js/main.103832c4.chunk.js"
+  },
+  {
+    "revision": "16fe4dd308fb9f0802e0",
+    "url": "/seminar-proto-2/static/js/runtime-main.3d7c96e1.js"
+  }
+]);
